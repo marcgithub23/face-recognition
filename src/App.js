0c653d38c21b76ca90bffefc3806d35a37ee34e6
marcgithub23 <marc.dagohoy2@gmail.com>
2024-09-25 13:42:45 +0100

    Add logo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      {/* <Logo />
-      <ImageLinkForm />
-      <FaceRecognition /> */}
+      <Logo />
+      {/* <ImageLinkForm /> */}
+      {/* <FaceRecognition /> */}
     </div>
   );
 }
---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Tilt from 'react-parallax-tilt';
+import brain from './brain.png';
+import './Logo.css'
+
+const Logo = () => {
+    return (
+        <div className='ma4 mt0'>
+            <Tilt
+            className='Tilt br2 shadow-2'
+            style={{ height: '150px', width: '150px', backgroundColor: 'darkgreen' }}
+            >
+                <div className='pa3'>
+                    <img src={brain} alt='logo of brain' style={{paddingTop: '5px'}} />
+                </div>
+            </Tilt>
+        </div>
+    )
+}
+
+export default Logo;
